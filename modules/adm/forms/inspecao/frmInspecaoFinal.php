7d8cde85f5f6decc008082d0638ec2ba896f5df0
eder <eder.ifbaiano@ymail.com>
2013-03-22 09:27:32 -0300

    992_atualizacao_1_22-03-2013

---
+++ modules/adm/forms/inspecao/frmInspecaoFinal.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of frmInspecaoFinal
+ *
+ * @author eder
+ */
+class frmInspecaoFinal {
+    //put your code here
+}
+
+?>
