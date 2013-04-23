977ad567e54f187a6e5996b949311628cb9ac123
eder <eder.ifbaiano@ymail.com>
2013-04-23 19:03:32 -0300

    Mensagem

--- html/relatorios2/repMapInspecaoVeiculo.php
+++ html/relatorios2/repMapInspecaoVeiculo.php
@@ -4,7 +4,8 @@ $idVeiculo = $_GET['idVeiculo'];
 $idMotorista = $_GET['idMotorista'];
 $datainicio = $_GET['datainicio'];
 $datafim = $_GET['datafim'];
+$idIsntituicao = $_GET['idIsntituicao'];
 
-header("Location: view/requisicoes/vw_mapReqInspecaoVeiculo.php?idVeiculo=$idVeiculo&idMotorista=$idMotorista&datainicio=$datainicio&datafim=$datafim");
+header("Location: view/requisicoes/vw_mapReqInspecaoVeiculo.php?idVeiculo=$idVeiculo&idMotorista=$idMotorista&datainicio=$datainicio&idIsntituicao=$idIsntituicao");
 
 ?>
--- html/relatorios2/view/requisicoes/vw_mapReqInspecaoVeiculo.php
+++ html/relatorios2/view/requisicoes/vw_mapReqInspecaoVeiculo.php
@@ -1,65 +1,69 @@
 <?php
+ini_set('memory_limit', '128M');
 ob_start();
-//ini_set('display_errors', 1);
 require_once '../../model/DAO/Conexao.php';
 require_once '../../controller/DataHelper.php';
-
-$idVeiculo = $_GET['idVeiculo'];
-$ididMotorista = $_GET['idMotorista'];
+$idInspecao = $_GET['idInspecao'];
+$status = $_GET['status'];
+$idMotorista = $_GET['idMotorista'];
 $datainicio = $_GET['datainicio'];
-$datafim = $_GET['datafim'];
-$dataAtual = date('d/m/Y');
-$arrData1 = explode('/', $datafim);
-$newDateFim = $arrData1 [2].'-'.$arrData1 [1].'-'.$arrData1 [0];
-
-$arrData2 = explode('/', $dataAtual);
-$newDateAtual = $arrData2 [2].'-'.$arrData2 [1].'-'.$arrData2 [0];
-
+$idVeiculo = $_GET['idVeiculo'];
+$idInspecao = $_GET['idIsntituicao'];
 $arrData = explode('/', $datainicio);
 $newDate = $arrData [2].'-'.$arrData [1].'-'.$arrData [0];
 
-$sql = "
-select
-	distinct  TO_CHAR(r.datahorareq, 'DD/MM/YY HH:MM:SS')
-	as datahorareq, r.idrequisicao, p.nome,t.item2, i.ordem, e.nomelocal
-	as origem,e2.nomelocal as destino, v.modelo||' - '||v.placa as modeloplaca
-	from ad_requisicao r INNER JOIN cm_tabelageral t on r.status = t.item1
-	INNER JOIN ad_tiporeq o on o.idtiporeq = r.tiporequisicao
-	inner join ad_itinerario i on i.idrequisicao = r.idrequisicao
-	INNER JOIN ad_endereco e ON i.idenderecoorigem=e.idendereco INNER JOIN ad_endereco e2
-        ON i.idenderecodestino=e2.idendereco
-        inner join ad_itemreqveiculo it on r.idrequisicao = it.idrequisicao
-        inner join ad_motorista m on m.idmotorista = it.idmotorista
-        inner join cm_pessoa p on p.idpessoa = m.idpessoa 
-        inner join ad_veiculo v on v.placa = it.placa
-        inner join ad_veiculouo uo on v.placa = uo.placa
-        where o.idtiporeq = 4 and t.tabela = 'AD_ALMOXSTATUSREQ' ";
 
-if ($idVeiculo || $datainicio || $datafim) {
-    if ($idVeiculo) {
-        $sql.=" AND it.placa like '%$idVeiculo%'";
-    }
-    if ($datainicio && $datafim) {
-        $sql.=" AND r.datahorareq between '$newDate' and '$newDateFim'";
-    }else if($datainicio){
-        $sql.=" AND r.datahorareq between '$newDate' and '$newDateAtual'"; 
-    }else if($datafim){
-        $sql.=" AND r.datahorareq < '$newDateFim'"; 
-    }
     
-}
-$sql.=" ORDER BY idrequisicao DESC ";
+    
+
+        $sql = "
+SELECT ds_componente, f.ds_conformidade as c_inicio,f1.ds_conformidade as c_fim,ob_inicio, ob_fim, p.nome, v.modelo||' - '||v.placa as modeloplaca
+  FROM vei_inspecao i 
+inner join vei_inspecao_componente ic on i.id_inspecao = ic.id_inspecao 
+  inner join vei_componente c on c.id_componente = ic.id_componente
+inner join vei_conformidade f on f.id_conformidade = ic.id_conformidade_inicio  
+  inner join vei_conformidade f1 on f1.id_conformidade = ic.id_conformidade_fim 
+inner join ad_motorista m on i.id_motorista = m.idmotorista
+  inner join cm_pessoa p on p.idpessoa = m.idpessoa 
+inner join ad_veiculo v on i.id_veiculo = v.placa
+where i.id_inspecao = $idInspecao ";
+
+
+
+
+$sqli = "
+SELECT p.nome
+  FROM vei_inspecao i 
+inner join ad_motorista m on i.id_motorista = m.idmotorista
+  inner join cm_pessoa p on p.idpessoa = m.idpessoa 
+where i.id_inspecao = $idInspecao ";
+
+$sqla = "
+SELECT v.modelo||' - '||v.placa as modeloplaca
+  FROM vei_inspecao i 
+inner join ad_veiculo v on i.id_veiculo = v.placa
+  where i.id_inspecao = $idRequisicao ";
+
 
 try {
     $db = Conexao::getInstance()->getDB();
 
     $preparedStatment = $db->prepare($sql);
+    //$preparedStatment1 = $db->prepare($sqli);
+   // $preparedStatment2 = $db->prepare($sqla);
+   // $preparedStatment3 = $db->prepare($sqle);
 
 
     $preparedStatment->execute();
-
+    //$preparedStatment1->execute();
+    //$preparedStatment2->execute();
+    //$preparedStatment3->execute();
 
     $rows = $preparedStatment->fetchAll(PDO::FETCH_ASSOC);
+   // $rowsi = $preparedStatment1->fetchAll(PDO::FETCH_ASSOC);
+   //$rowsa = $preparedStatment2->fetchAll(PDO::FETCH_ASSOC);
+    //$rowse = $preparedStatment3->fetchAll(PDO::FETCH_ASSOC);
+
     Conexao::getInstance()->disconnect();
 } catch (Exception $e) {
     $e->getMessage();
@@ -69,14 +73,9 @@ try {
 $dataHelper = new DataHelper();
 
 $baseURL = 'http://' . $_SERVER['HTTP_HOST'];
-//$css = $baseURL.'/relatorios2/view/statics/css/estilo.css';
-$css0 = $baseURL . '/relatorios2/view/statics/css/estilo3.css';
-$css3 = $baseURL . '/relatorios2/view/statics/css/demo_table_jui.css';
-$css4 = $baseURL . '/relatorios2/view/statics/css/jquery-ui-1.8.21.custom.css';
+$css = $baseURL . '/relatorios2/view/statics/css/reqestilo.css';
+$css2 = $baseURL . '/relatorios2/view/statics/css/reqestilo2.css';
 
-$js1 = $baseURL . '/relatorios2/view/statics/js/jquery.js';
-$js2 = $baseURL . '/relatorios2/view/statics/js/jquery.dataTables.js';
-$js3 = $baseURL . '/relatorios2/view/statics/js/formatted-currency-asc.js';
 // Arquivo temporário que será utilizado para gerar o PDF.
 $tmpFile = tempnam('/tmp', 'pdf_');
 
@@ -85,21 +84,11 @@ $url = $baseURL . '/relatorios2/PRINT_PDF/print_pdf.php?input_file=' . rawurlenc
 
 
 $arraySize = count($rows);
-$titulo = "MAPA DE UTILIZAÇÃO DE VEÍCULO<br/>";
-$titulo1 = "MAPA DE UTILIZAÇÃO DE VEÍCULO";
-$veiculo = $rows[0]['modeloplaca']; 
 
- if ($idVeiculo != "") {
-    $titulo .= "VEÍCULO: " . $veiculo ." <br/>";
- }if($datainicio && $datafim){
-    $titulo .="PERÍODO: De ". $datainicio. " a ". $datafim; 
- }else if($datainicio){
-    $titulo .="PERÍODO: A partir de ". $datainicio; 
- }else if($datafim){
-    $titulo .="PERÍODO: Até ". $datafim; 
- }
-
-
+$titulo = " INSPEÇÃO DE  - VEÍCULOS<br/>";
+if ($idRequisicao != "") {
+    $titulo .= "INSPEÇÃO Nº: " . $idRequisicao;
+} 
 
 ?>
 
@@ -108,105 +97,62 @@ $veiculo = $rows[0]['modeloplaca'];
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-
-        <link rel="stylesheet" type="text/css" href="<?php echo $css0; ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php echo $css3; ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php echo $css4; ?>" />
-
-        <script type="text/javascript" src="<?php echo $js1 ?>"></script>
-        <style type="text/css" media="screen">
-
-            .dataTables_info { padding-top: 0; }
-            .dataTables_paginate { padding-top: 0; }
-            .css_right { float: right; }
-            #tabela_wrapper .fg-toolbar { font-size: 0.8em }
-            #theme_links span { float: left; padding: 2px 10px; }
-
-        </style>
-
-        <script type="text/javascript" src="<?php echo $js2 ?>"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo $css; ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo $css2; ?>" />
+        
         <script type="text/javascript" charset="utf-8">
-            $(document).ready( function() {
-                $('#tabela').dataTable( {					
-                        					
-                    "oLanguage": {
-
-                        "sSearch": "Procurar",
-                        "sLengthMenu": "Mostrar _MENU_ registros por página",
-                        "sZeroRecords": "Nada encontrado - 0",
-                        "sInfo": "Mostrando _START_ até _END_ de _TOTAL_ Registros",
-                        "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-                        "sInfoFiltered": "(Filtrado de _MAX_ registros no total)"
-                    },
-                    "bJQueryUI": true,
-                    "aLengthMenu": [[-1, 10, 25, 50,100,200,500,1000,5000], ['Todos', 10, 25, 50,100,200,500,1000,5000]],
-                    "iDisplayLength": -1,
-                    "sPaginationType": "full_numbers",
-                    "aaSorting": [],                                        
-                    "aoColumnDefs": [  //{ "bSortable": false, "aTargets": [ 4 ] } ,
-                       {  "sType": "date-uk", "aTargets": [ 0 ]},
-                       {  "sType": "currency", "aTargets": [  ]},
-                       {  "sType": "currency", "aTargets": [ 4 ]},
-                        //                                                         {  "sType": "formatted-num", "aTargets": [  ]}                                                         
-                    ]
-                         
-                } );
-            } );
+            
         </script>
-        </script>
-        <title><?php echo $titulo1; ?></title>                    
+        
+        <title><?php echo $titulo; ?></title>
     </head>
-    <body align="center">
+    <body>
 
-<?php //ob_start();     ?>
+<?php //ob_start();  ?>
         <div id="conteudo">
 
-<?php require_once '../statics/cabecalho.php'; ?>
-<!--            <div id="menu">
+        <?php include_once '../statics/cabecalho.php'; ?>
+
+            <div id="menu">
                 <a onclick="javascript:history.go(-1);">Voltar&nbsp&nbsp&nbsp&nbsp&nbsp</a><br/>
-                <a href="<?php // echo $url; ?>">Imprimir Relatório <img src="../statics/img/action_print.gif" alt="Imprimir Relatório" /></a>
-            </div>-->
-            <div id="menu"><br/></div>
-            <table cellpadding="0" cellspacing="0" border="0" class="display" id="tabela" style="width: 100%">
-                <thead> 
-                    <tr>                                
-                        <td class="data">DATA / HORA</td>
-                        <td class="descricao">REQUISIÇÃO</td>
-                        <td class="descricao">ORIGEM</td>
-                        <td class="descricao">DESTINO</td>
-                        <td class="descricao">MOTORISTA</td>
-                        <td class="valores">SITUAÇÃO</td>
-                    </tr>
-                </thead> 
-                <tbody>
-<?php
-for ($i = 0; $i < count($rows); $i++) {
-    //$saldoTotal += $rows[$i]['valor'];
-    ?>                              
-                        <tr>                                     
-                            <td class="valores" style="text-align: center;"><?php echo $rows[$i]['datahorareq']; ?></td>
-                            <td style="width: 7em" class="valores" style="text-align: center;"><?php echo $rows[$i]['idrequisicao']; ?></td>
-                            <td class="valores" style="text-align: center;"><?php echo $rows[$i]['origem']; ?></td>
-                            <td class="valores" style="text-align: center;"><?php echo $rows[$i]['destino']; ?></td>
-                            <td class="valores" style="text-align: center;"><?php echo $rows[$i]['nome']; ?></td>
-                            <td class="valores" style="text-align: left;"><?php echo $rows[$i]['item2']; ?></td>
-                        </tr>                            
-    <?php
-}
-?>
-                    <tfoot style="background-color: #D1CFD0;">                                    
-                        <td class="valores" style="text-align: left;">--</td>
-                        <td class="valores" style="text-align: center;">--</td>
-                        <td class="valores" style="text-align: center;">--</td>
-                        <td class="valores" style="text-align: center;">--</td>
-                        <td class="valores" style="text-align: center;">--</td>
-                        <td class="valores" style="text-align: right;"><?php echo number_format($saldoTotal, 2, ',', '.'); ?></td>                                   
-                    </tfoot>                         
-                </tbody>    
-            </table>                        
+                <a href="<?php echo $url; ?>">Imprimir Relatório <img src="../statics/img/action_print.gif" alt="Imprimir Relatório" /></a>
+            </div>
+          <table border="1" width="100%">
+            <tr>
+                    <td style="text-align: center;">
+                        <div style="border-width:medium; border-color:#000" align="center">
+                            <h3>ITINERÁRIO</h3></div> 
+                        <table align="center"  width="100%" border="1">
+                <tr> 
+                    <td width="25%" style="text-align: left;"><b>COMPONENTE</b></td>
+                    <td width="15%" style="text-align: center;"><b>CONFORMIDADE INÍCIO<b/></td> 
+                    <td width="15%" style="text-align: center;"><b>CONFORMIDADE FIM<b/></td>
+                    <td width="20%" style="text-align: center;"><b>OBS INÍCIO<b/></td>
+                    <td width="20%" style="text-align: center;"><b>OBS: FIM<b/></td>
+                </tr>
+            <?php
+               for ($i = 0; $i < count($rows); $i++) {
+            ?>   
+                   <tr>
+                            <td style="text-align: left;"><?php echo $rows[$i]['ds_componente']; ?></td>
+                            <td style="text-align: center;"><?php echo $rows[$i]['c_inicio']; ?></td>
+                            <td style="text-align: center;"><?php echo $rows[$i]['c_fim']; ?></td>
+                            <td style="text-align: center;"><?php echo $rows[$i]['ob_inicio']; ?></td>
+                            <td style="text-align: center;"><?php echo $rows[$i]['ob_fim']; ?></td>
+                </tr> 
+                  <!-- <h3 align="left">HORÁRIO: <?php //echo $rowsi[$i]['datasaida']; ?>  ORIGEM: <?php //echo $rowsi[$i]['origem']; ?> - DESTINO: <?php //echo $rowsi[$i]['destino']; ?></h3>-->
+                      
+            <?php
+               }
+            ?>
+                </table>
+                    </td>
+                </tr>
+            </table>  
+        
         </div>
     </body>
 </html>
+
 <?php file_put_contents($tmpFile, ob_get_contents()); ?>
 
-<?php 
